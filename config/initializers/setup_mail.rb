d0f5eeaea0a2e0d5838be030a6da2ec4d3d13cea
adamzuckerberg <adam@adamzuckerberg.com>
2015-04-27 08:08:52 -0700

    Production email set. Initializer email removed

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,9 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development? || Rails.env.production?
-# remove the "|| Rails.env.production?" when ready to go live
 
 
 
