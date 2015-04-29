e770d206ba6a3cbf29522c1852f24dbb7912db7e
adamzuckerberg <adam@adamzuckerberg.com>
2015-04-29 08:18:41 -0700

    DevelopmentMailInterceptor removed

--- app/models/development_mail_interceptor.rb
+++
@@ -1,7 +0,0 @@
-# class DevelopmentMailInterceptor
-# 	def self.delivering_email(message)
-# 		message.subject = "#{message.to} #{message.subject}" 
-# 		message.to = ENV["ADMINISTRATOR_EMAIL"] || Rails.application.secrets[:ADMINISTRATOR_EMAIL]
-# 	end
-# end
-
