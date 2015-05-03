e258c7df5db4cbcf8f16113ef7aa0937d418c08d
adamzuckerberg <adam@adamzuckerberg.com>
2015-05-03 16:56:30 -0700

    Redirect if user.confirmed?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,8 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     # first-time users stay on page until confirmed
-    if flash[:notice] = 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
-        redirect_to register_path
-    else
-    	redirect_to root_url
+    if user.confirmed?
+      redirect_to root_url
     end
   end
 
