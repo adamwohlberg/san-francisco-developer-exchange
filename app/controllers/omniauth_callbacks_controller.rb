73c0248c5118e0a8d31cc1eda6c53a70e8ae4792
adamzuckerberg <adam@adamzuckerberg.com>
2015-05-05 08:26:58 -0700

    Update FB login flow

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  before_filter :check_user_exists
+  # before_filter :check_user_exists
 
   def all
     user = User.from_omniauth(request.env["omniauth.auth"], request.env["omniauth.params"])
@@ -17,18 +17,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   private
 
-  def check_user_exists
-    auth = request.env["omniauth.auth"]
-    u = User.where(provider: auth.provider, uid: auth.uid).first
-    if request.env["omniauth.auth"] && request.env["omniauth.params"].blank? && u.blank?
-      if URI(request.referer).path == '/login'
-        session[:errors] = "You need to register a new account before you can sign in here."
-        redirect_to URI(request.referer).path
-      else
-        session[:errors] = "You have already signed up with this email."
-        redirect_to URI(request.referer).path
-      end
-    end  
+  # def check_user_exists
+  #   auth = request.env["omniauth.auth"]
+  #   u = User.where(provider: auth.provider, uid: auth.uid).first
+  #   if request.env["omniauth.auth"] && request.env["omniauth.params"].blank? && u.blank?
+  #     if URI(request.referer).path == '/login'
+  #       session[:errors] = "You need to register a new account before you can sign in here."
+  #       redirect_to URI(request.referer).path
+  #     else
+  #       session[:errors] = "You have already signed up with this email."
+  #       redirect_to URI(request.referer).path
+  #     end
+  #   end  
   end
 
 end
