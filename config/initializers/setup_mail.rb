5b361488a0b328738f7548c8f7380558df54dc7b
adamzuckerberg <adam@adamzuckerberg.com>
2015-05-15 09:26:09 -0700

    Install SendGrid

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,22 @@
-
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "SanFranciscoDeveloperExchange.com",
-  :user_name            => ENV['GMAIL_USERNAME'],
-  :password             => ENV['GMAIL_PASSWORD'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "SanFranciscoDeveloperExchange.com",
+#   :user_name            => ENV['GMAIL_USERNAME'],
+#   :password             => ENV['GMAIL_PASSWORD'],
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
 
 # ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development? || Rails.env.production?
 
 
-
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'sanfranciscodeveloperexchange.com',
+  :enable_starttls_auto => true
+}
