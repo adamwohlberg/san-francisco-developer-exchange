4d4a6209bb4b250e1b24de28cfeef43b06294edb
adamzuckerberg <adam@adamzuckerberg.com>
2015-05-16 00:10:00 -0700

    Updated notes.  Rollback changes to add multiple attachments with Paperclip gem in contract form for contract-specific profile image separate from employer image

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -60,7 +60,7 @@ class Developer < User
   acts_as_paranoid
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/noface"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-    validates_attachment_size :avatar, :in => 0.megabytes..2.megabytes
+    validates_attachment_size :avatar, :in => 0.megabytes..3.megabytes
 
   has_and_belongs_to_many :skills, join_table: :developers_skills
 
